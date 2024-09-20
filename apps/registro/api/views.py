adbe893503b568cc6cb3fe67e3368e39bc1bc71b
maibercordoba <cordobamaiber@gmail.com>
2024-09-20 10:58:33 -0500

    creando en las vistas de la app registro el post y cambiando en el modelo en el campo fechaIngreso a tipo de dato fecha por que lo tenia en tipo de dato hora

--- apps/registro/api/views.py
+++ apps/registro/api/views.py
@@ -9,3 +9,9 @@ class RegistroApiView(APIView):
     def get(self,request):
         serializer = RegistroSerializer(Registro.objects.all(), many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+    
+    def post(self,request):
+        serializer = RegistroSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
--- apps/registro/models.py
+++ apps/registro/models.py
@@ -7,7 +7,7 @@ class Registro(models.Model):
     nombre = models.CharField(max_length=50)
     categoria = models.CharField(max_length=100)
     cantidad = models.IntegerField()
-    fechaIngreso =models.TimeField(auto_now=True)
+    fechaIngreso =models.DateField(auto_now=True)
 
     def __str__(self):
         return self.nombre 
