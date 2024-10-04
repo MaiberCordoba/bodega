8aa7617d0e456361688192811e96824a15127270
maibercordoba <cordobamaiber@gmail.com>
2024-10-04 11:36:40 -0500

    haciendo uso de modelViewSet en la app registro para ahorrar lineas de codigo

--- apps/registro/api/router.py
+++ apps/registro/api/router.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from apps.registro.api.views import RegistroApiSet
+from apps.registro.api.views import RegistroModelViewSet
 
 router_registro = DefaultRouter()
-router_registro.register(prefix="registro", basename="registro", viewset=RegistroApiSet)
+router_registro.register(prefix="registro", basename="registro", viewset=RegistroModelViewSet)
--- apps/registro/api/views.py
+++ apps/registro/api/views.py
@@ -1,53 +1,59 @@
-#from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet
-from rest_framework.response import Response
-from rest_framework import status
+#1 metodo#from rest_framework.views import APIView
+#2 metodo#from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
+#from rest_framework.response import Response
+#from rest_framework import status
 from apps.registro.models import Registro
 from apps.registro.api.serializers import RegistroSerializer
 
-class RegistroApiSet(ViewSet):
+class RegistroModelViewSet(ModelViewSet):
+    serializer_class = RegistroSerializer
+    queryset = Registro.objects.all()
 
-    #def get(self,request):
-    def list(self,request):
-    
-        serializer = RegistroSerializer(Registro.objects.all(), many=True)
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
-    
-    def retrieve(self,request,pk=int):
-        try:
-            serializer = RegistroSerializer(Registro.objects.get(pk=pk))
-            return Response(status=status.HTTP_200_OK, data=serializer.data)
-        except Registro.DoesNotExist:
-            return Response(status=status.HTTP_400_BAD_REQUEST,data=("dato no encotrado"))
-        
-    #def post(self,request):
-    def create(self,request):
-        serializer = RegistroSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
-    
-    def update(self,request,pk):
-        try:
-            serializer = Registro.objects.get(pk=pk)
-            serializer = RegistroSerializer(serializer,data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(status=status.HTTP_200_OK,data=serializer.data)
-        except Registro.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND,data=("salida no encontrada."))
-        
-    def partial_update(self,request,pk):
-        try:
-            serializer = Registro.objects.get(pk=pk)
-            serializer = RegistroSerializer(serializer,data=request.data, partial=True)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(status=status.HTTP_200_OK,data=serializer.data)
-        except Registro.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND,data=("salida no encontrada."))
-        
-    def destroy(self,request,pk):
-            serializer = Registro.objects.get(pk=pk)
-            serializer.delete()
-            return Response(status=status.HTTP_200_OK)
+    #1 forma def get(self,request):
+    #2def list(self,request):
+    #2
+    #2    serializer = RegistroSerializer(Registro.objects.all(), many=True)
+    #2    return Response(status=status.HTTP_200_OK, data=serializer.data)
+    #2
+    #2def retrieve(self,request,pk=int):
+    #2    try:
+    #2        serializer = RegistroSerializer(Registro.objects.get(pk=pk))
+    #2        return Response(status=status.HTTP_200_OK, data=serializer.data)
+    #2    except Registro.DoesNotExist:
+    #2        return Response(status=status.HTTP_400_BAD_REQUEST,data=("dato no encotrado"))
+    #2    
+    #1 forma def post(self,request):
+    #2def create(self,request):
+    #2    try:
+    #2        serializer = RegistroSerializer(data=request.data)
+    #2        serializer.is_valid(raise_exception=True)
+    #2        serializer.save()
+    #2        return Response(status=status.HTTP_200_OK, data=serializer.data)
+    #2    except:
+    #2        return Response(status=status.HTTP_404_NOT_FOUND,data=("error al crear nuevos datos"))
+    #2
+    #2def update(self,request,pk):
+    #2    try:
+    #2        serializer = Registro.objects.get(pk=pk)
+    #2        serializer = RegistroSerializer(serializer,data=request.data)
+    #2        serializer.is_valid(raise_exception=True)
+    #2        serializer.save()
+    #2        return Response(status=status.HTTP_200_OK,data=serializer.data)
+    #2    except Registro.DoesNotExist:
+    #2        return Response(status=status.HTTP_404_NOT_FOUND,data=("salida no encontrada."))
+    #2    
+    #2def partial_update(self,request,pk):
+    #2    try:
+    #2        serializer = Registro.objects.get(pk=pk)
+    #2        serializer = RegistroSerializer(serializer,data=request.data, partial=True)
+    #2        serializer.is_valid(raise_exception=True)
+    #2        serializer.save()
+    #2        return Response(status=status.HTTP_200_OK,data=serializer.data)
+    #2    except Registro.DoesNotExist:
+    #2        return Response(status=status.HTTP_404_NOT_FOUND,data=("salida no encontrada."))
+    #2    
+    #2def destroy(self,request,pk):
+    #2        serializer = Registro.objects.get(pk=pk)
+    #2        serializer.delete()
+    #2        return Response(status=status.HTTP_200_OK)
